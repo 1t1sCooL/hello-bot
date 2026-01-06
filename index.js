a95787f6c981e3da9ec74760f37bccff66ccb34c
1t1sCooL <mmalabugin@gmail.com>
2026-01-07 01:57:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Telegraf } = require("telegraf");
+
+const token = process.env.HELLO_BOT_TOKEN;
+
+if (!token) {
+  console.error("ERROR: BOT_TOKEN is not defined!");
+  process.exit(1);
+}
+
+const bot = new Telegraf("YOUR_BOT_TOKEN");
+
+bot.command("help", (ctx) => {
+  ctx.reply(`
+    Бот может здороваться на разных языках.
+    Список поддерживаемых приветствий:
+    - привет - русский
+    - hello - английский
+    - hola - испанский
+    `);
+});
+
+bot.hears("привет", (ctx) => ctx.reply("привет"));
+bot.hears("hello", (ctx) => ctx.reply("hello"));
+bot.hears("hola", (ctx) => ctx.reply("hola"));
+
+bot.on("text", (ctx) =>
+  ctx.reply(`Приветствие "${ctx.update.message.text}" не поддерживается.`)
+);
+
+bot.launch().then(() => console.log("Started"));
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
